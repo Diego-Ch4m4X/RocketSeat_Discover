3b207e04030b79f730a433cbe79872a5433e4070
Diego Leite <diego.santos.leite@hotmail.com>
2026-01-29 00:50:57 -0300

    Correção modo de apresentacao

--- script.js
+++ script.js
@@ -1,63 +1,22 @@
-function toggleMode() {
+// Aplica o tema completo (classe + avatar)
+function applyTheme(isLight) {
+  const html = document.documentElement
+  const img = document.querySelector("#profile img")
 
-    const html = document.documentElement
-    html.classList.toggle('light')
- //   if (html.classList.contains('light')) {
- //       html.classList.remove('light')
- //   } else { 
- //       html.classList.add('light')
- //   }
-
-    const img = document.querySelector("#profile img")
-
-    if (html.classList.contains('light')){
-        img.setAttribute('src', './img/avatar-light.png')
-    }
-    else {
-        img.setAttribute('src', './img/avatar.png')
-    }
-    
-
-
-
- 
+  if (isLight) {
+    html.classList.add("light")
+    img.setAttribute("src", "./img/avatar-light.png")
+  } else {
+    html.classList.remove("light")
+    img.setAttribute("src", "./img/avatar.png")
+  }
 }
 
+// Alterna o tema ao clicar no switch
+function toggleMode() {
+  const isLight = document.documentElement.classList.toggle("light")
+  applyTheme(isLight)
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Estado inicial do tema (dark por padrão)
+applyTheme(false)
